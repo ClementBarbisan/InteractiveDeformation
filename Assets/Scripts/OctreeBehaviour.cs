6f4668102811976d8b02dff1c432ccfb4d10dfc5
clement barbisan <c.barbisan@free.fr>
2021-05-09 13:08:15 +0200

    test with more complex mesh

--- Assets/Scripts/OctreeBehaviour.cs
+++ Assets/Scripts/OctreeBehaviour.cs
@@ -23,7 +23,8 @@ public class OctreeBehaviour : MonoBehaviour
     private int _kernelId;
     private ComputeBuffer _buffInitNormal;
     private List<Vector3> _initNormal;
-    [SerializeField] private float _scale = 0.1f;
+    [SerializeField] private float _drag = 0.1f;
+    [SerializeField] private float _elasticity = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -39,40 +40,40 @@ public class OctreeBehaviour : MonoBehaviour
         _buffInitPos = new ComputeBuffer(_countVertices, sizeof(float) * 3);
         _buffInitNormal = new ComputeBuffer(_countVertices, sizeof(float) * 3);
         _buffPos = new ComputeBuffer(_countVertices, sizeof(float) * 3);
-        _impactPos = new ComputeBuffer(_countVertices, sizeof(float) * 3);
-        // _buffColor = new ComputeBuffer(_meshFilter.mesh.colors.Length, sizeof(float) * 4);
+        _impactPos = new ComputeBuffer(_countVertices, sizeof(float) * 3); 
+        _buffColor = new ComputeBuffer(_countVertices, sizeof(float) * 4);
         _kernelId = _computeKernel.FindKernel("CSMain");
         _computeKernel.SetBuffer(_kernelId, "_VertexBuffer", _buffPos);
         _computeKernel.SetBuffer(_kernelId, "_InitialPositionBuffer", _buffInitPos);
         _computeKernel.SetBuffer(_kernelId, "_InitialNormalBuffer", _buffInitNormal);
         _computeKernel.SetBuffer(_kernelId, "_ImpactPositionBuffer", _impactPos);
-        // _computeKernel.SetBuffer(_kernelId, "_ColorsBuffer", _buffColor);
+        _computeKernel.SetFloat("_drag", _drag);
+        _computeKernel.SetFloat("_elacticity", _elasticity);
+        _computeKernel.SetBuffer(_kernelId, "_ColorsBuffer", _buffColor);
         _meshFilter = GetComponent<MeshFilter>();
-        _octreePoints = new BoundsOctree<int>(1, Vector3.zero, 0.01f, 2);
+        _octreePoints = new BoundsOctree<int>(1, Vector3.zero, 0.01f, 1);
         Mesh tmpMesh = _meshFilter.mesh;
-        // for (int i = 0; i < tmpMesh.colors.Length; i++)
-        // {
-        //     _colors.Add(tmpMesh.colors[i]);
-        // }
-        // Debug.Log(_colors.Count);
+        MeshRenderer render = GetComponent<MeshRenderer>();
         for (int i = 0; i < tmpMesh.vertexCount; i++)
         {
             _initPos.Add(tmpMesh.vertices[i]);
             _pos.Add(tmpMesh.vertices[i]);
             _initNormal.Add(tmpMesh.normals[i]);
             _impactList.Add(Vector3.zero);
+            _colors.Add(((Texture2D)render.material.mainTexture).GetPixel(i % render.material.mainTexture.width, i / render.material.mainTexture.height));
             _octreePoints.Add(i, new Bounds(Vector3.Scale(tmpMesh.vertices[i], transform.localScale) + transform.position, Vector3.one * 0.01f));
         }
         _buffInitPos.SetData(_initPos.ToArray());
         _buffInitNormal.SetData(_initNormal.ToArray());
         _buffPos.SetData(_pos.ToArray());
-        //_buffColor.SetData(_colors.ToArray());
+        _buffColor.SetData(_colors.ToArray());
         _meshFilter.mesh.MarkDynamic();
+        
     }
 
     private void OnApplicationQuit()
     {
-        //_buffColor.Release();
+        _buffColor.Release();
         _buffPos.Release();
         _buffInitPos.Release();
         _buffInitNormal.Release();
@@ -107,12 +108,8 @@ public class OctreeBehaviour : MonoBehaviour
         _octreePoints.GetColliding(tmpIndexes, new Bounds(other.transform.position, other.transform.localScale / 5));
         foreach (int index in tmpIndexes)
         {
-            Vector3 normOther = other.transform.position;
-            Vector3 normInit =_initPos[index];
-            _impactList[index] = (1f / Vector3.Distance(other.transform.position, transform.position)) *
-                new Vector3( 1f - (normOther.x - normInit.x),
-                    1f - (normOther.y - normInit.y),
-                    1f - (normOther.z - normInit.z) ) / 2 * other.rigidbody.mass;
+            _impactList[index] = other.transform.position;
+           
         }
     }
 
@@ -122,22 +119,18 @@ public class OctreeBehaviour : MonoBehaviour
         _octreePoints.GetColliding(tmpIndexes, new Bounds(other.transform.position, other.transform.localScale / 5));
         foreach (int index in tmpIndexes)
         {
-            Vector3 normOther = other.transform.position;
-            Vector3 normInit = _initPos[index];
-            _impactList[index] = Mathf.Pow(1f / (Vector3.Distance(other.transform.position, transform.position)), 2) *
-                new Vector3( 1f - (normOther.x - normInit.x),
-                    1f - (normOther.y - normInit.y),
-                    1f - (normOther.z - normInit.z)) / 2 * other.rigidbody.mass;
+            _impactList[index] = other.transform.position;
+          
         }
     }
     
     private void OnCollisionExit(Collision other)
     {
-        // List<int> tmpIndexes = new List<int>();
-        // _octreePoints.GetColliding(tmpIndexes, new Bounds(other.transform.position, other.transform.localScale));
-        // foreach (int index in tmpIndexes)
-        // {
-        //     _impactList[index] = Vector3.zero;
-        // }
+        List<int> tmpIndexes = new List<int>();
+        _octreePoints.GetColliding(tmpIndexes, new Bounds(other.transform.position, other.transform.localScale / 5));
+        foreach (int index in tmpIndexes)
+        {
+            _impactList[index] = Vector3.zero;
+        }
     }
 }
